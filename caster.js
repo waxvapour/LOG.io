d5cea69cbf6d190534d4620cdc107b83502da2ef
vaibhav sharma <waxvapour@gmail.com>
2017-12-25 02:11:18 +0530

    first copy
    tempelete site

---
+++ caster.js
@@ -0,0 +1,12 @@
+/*
+__caster 
+by, Waxvapour@gmail.com
+*/
+function a()
+{
+	alert("just a demo, this should open up the real thing in the real site!");
+}
+function faceoff()
+{
+	alert("Sir, this site is purely client_site and no server_site programming had done for this page as the page was intended to be a example, also the content on the site is out of my imagination only and will be corrected on the real site. ");
+}
